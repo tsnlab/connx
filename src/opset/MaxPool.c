fd8a06ef9b4882938d39b81c64df3bd13c4e0b48
jisu.choi <pblwork1@gmail.com>
2021-08-30 14:40:47 +0900

    Remove unnecessary x_padded_slice

--- src/opset/MaxPool.c
+++ src/opset/MaxPool.c
@@ -154,6 +154,7 @@ int MaxPool(connx_Graph* graph, uint32_t output_count, uint32_t* outputs, __attr
     connx_Slice slices[X->ndim];
 
     int32_t units[2 + feature_dim]; // batch_unit, channel_unit, feature units
+
     units[X->ndim - 1] = 1;
 
     for (int32_t i = X->ndim - 2; i >= 0; i--) {
@@ -188,13 +189,10 @@ int MaxPool(connx_Graph* graph, uint32_t output_count, uint32_t* outputs, __attr
                 connx_Slice_init(&slices[0], batch, batch + 1, 1, batch);
                 connx_Slice_init(&slices[1], channel, channel + 1, 1, channel);
 
-                int32_t d_idx[feature_dim];
-                int32_t k_idx[feature_dim];
-                int32_t k_units[feature_dim] = {1, }; // batch_unit, channel_unit, feature units
-
                 while (connx_Iterator_next(&x_iter)) {
                     TEMPLATE_TYPE y = 0;
                     int64_t argmax_idx = -1;
+                    int32_t k_idx[feature_dim];
 
                     // Make slicers for copy patch of X[batch, channel] on to padded X
                     for (int32_t i = 0; i < feature_dim; i++) {
@@ -210,10 +208,10 @@ int MaxPool(connx_Graph* graph, uint32_t output_count, uint32_t* outputs, __attr
 
                     // Get patch of X[batch, channel]
                     connx_Tensor* x_patch = connx_Tensor_get_by_slice(X, slices);
-                    int32_t tmp_size = connx_Int32_product(feature_dim, x_patch->shape + feature_dim);
+                    int32_t tmp_size = connx_Int32_product(feature_dim, x_patch->shape + 2);
                     int32_t kernel_offset = connx_TEMPLATE_NAME_argmax(tmp_size, &y, x_patch->buffer);
 
-                    // Convert kernel argmax offset to index
+                    // Convert offset to index
                     for (int32_t i = 1; i < feature_dim; i++) {
                         int32_t unit = connx_Int32_product(feature_dim - i, x_patch->shape + 2 + i);
                         k_idx[i - 1] += kernel_offset / unit;
@@ -221,8 +219,10 @@ int MaxPool(connx_Graph* graph, uint32_t output_count, uint32_t* outputs, __attr
                     }
                     k_idx[feature_dim - 1] += kernel_offset;
 
-                    // Compute offset of max argmax in X[batch, channel]
+                    int32_t d_idx[feature_dim];
                     int32_t d_offset = 0;
+
+                    // Compute offset of max argmax in X[batch, channel]
                     for (int32_t i = 0; i < feature_dim; i++) {
                         d_idx[i] = x_iter.slices[i].idx + k_idx[i];
                         d_offset += d_idx[i] * units[2 + i];
