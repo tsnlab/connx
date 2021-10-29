6ca9c2f5becfd725c99713fa30d59490947b7743
Semih Kim <semih.kim@gmail.com>
2021-10-29 13:28:46 +0900

    Platform dependent thread and lock code is added to hal.h

--- include/connx/hal.h
+++ include/connx/hal.h
@@ -18,9 +18,13 @@
 #ifndef __CONNX_HAL_H__
 #define __CONNX_HAL_H__
 
-#include <pthread.h>
 #include <stdbool.h>
 #include <stdint.h>
+
+// Ref: https://iq.opengenus.org/detect-operating-system-in-c/
+#ifdef __linux__
+#include <pthread.h>
+#endif /* __linux__ */
 
 #define CONNX_ALIGNMENT 16 // Data alignment
 #define CONNX_ALIGN(offset) (((offset) + CONNX_ALIGNMENT - 1) & ~(CONNX_ALIGNMENT - 1))
@@ -42,7 +46,9 @@ int32_t connx_read(void* buf, int32_t size);
 int32_t connx_write(void* buf, int32_t size);
 
 // Lock
+#ifdef __linux__
 typedef pthread_mutex_t connx_Lock;
+#endif /* __linux__ */
 
 void connx_Lock_init(connx_Lock* lock);
 void connx_Lock_destroy(connx_Lock* lock);
@@ -51,7 +57,9 @@ void connx_Lock_lock(connx_Lock* lock);
 void connx_Lock_unlock(connx_Lock* lock);
 
 // Thread pool
+#ifdef __linux__
 typedef pthread_t connx_Thread;
+#endif /* __linux__ */
 
 uint32_t connx_Thread_alloc(uint32_t count, connx_Thread* threads);
 void connx_Thread_free(uint32_t count, connx_Thread* threads);
