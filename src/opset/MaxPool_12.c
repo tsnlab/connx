d0ab9f5a8c451e25bc49f78420eded6e19141877
Semih Kim <semih.kim@gmail.com>
2022-04-01 20:30:06 +0900

    Remove unnecessary codes

--- src/opset/MaxPool_12.c
+++ src/opset/MaxPool_12.c
@@ -233,34 +233,6 @@ int MaxPool_{{op_version}}(connx_Graph* graph, uint32_t output_count, uint32_t* 
         Indices_array = (int64_t*)Indices->buffer;
     }
 
-    int32_t y_idx = 0;
-
-    int32_t storage_unit = 0;
-    if (storage_order == 1) {
-        if (feature_dim >= 2) {
-            storage_unit = output_shape[feature_dim - 2] * output_shape[feature_dim - 1];
-        } else {
-            storage_unit = 0;
-        }
-    }
-
-    // Figure out dilated kernel shape
-    int32_t new_kernel_shape[feature_dim];
-    for (int32_t i = 0; i < feature_dim; i++) {
-        new_kernel_shape[i] = dilations[i] * kernel_shape[i];
-        if (dilations[i] != 1) {
-            new_kernel_shape[i] -= 1;
-        }
-    }
-
-    int32_t units[2 + feature_dim]; // batch_unit, channel_unit, feature units
-
-    units[X->ndim - 1] = 1;
-
-    for (int32_t i = X->ndim - 2; i >= 0; i--) {
-        units[i] = units[i + 1] * X->shape[i + 1];
-    }
-
     int32_t batch_count = X->shape[0];
     int32_t channel_count = X->shape[1];
     int32_t X_unit = connx_Int32_product(feature_dim, X->shape + 2);
@@ -291,124 +263,6 @@ int MaxPool_{{op_version}}(connx_Graph* graph, uint32_t output_count, uint32_t* 
                     connx_error("MaxPool: Feature dimension %d is not supported yet.\n", feature_dim);
                     return CONNX_NOT_SUPPORTED_DATATYPE;
                 }
-
-                // x_iter
-                connx_Slice x_slices[feature_dim];
-                for (int32_t i = 0; i < feature_dim; i++) {
-                    connx_Slice_set(&x_slices[i], -pads[i], -pads[i] + output_shape[i] * strides[i], strides[i]);
-                }
-
-                connx_Iterator x_iter;
-                connx_Iterator_init(&x_iter, feature_dim, x_slices);
-
-                while (connx_Iterator_next(&x_iter, 1)) {
-                    // clang-format off
-                    {{TYPE}} y = 0;
-                    // clang-format on
-                    int64_t argmax_idx = -1;
-                    int32_t k_idx[feature_dim];
-
-                    // x_patch_iter
-                    connx_Slice x_patch_slices[feature_dim];
-
-                    int32_t x_idxs[feature_dim];
-                    connx_Iterator_indices(&x_iter, x_idxs);
-
-                    for (int32_t i = 0; i < feature_dim; i++) {
-                        int32_t x_patch_start = x_idxs[i];
-                        int32_t x_patch_end = x_idxs[i] + kernel_shape[i] * dilations[i];
-                        int32_t x_patch_step = dilations[i];
-
-                        if (x_patch_start < 0) {
-                            x_patch_start = (x_idxs[i] + -x_idxs[i] * dilations[i]) % dilations[i];
-                        }
-
-                        if (x_patch_end > feature_shape[i]) {
-                            x_patch_end = feature_shape[i];
-                        }
-
-                        connx_Slice_set(&x_patch_slices[i], x_patch_start, x_patch_end, x_patch_step);
-
-                        int32_t x_padded_start = x_idxs[i] < 0 ? -x_idxs[i] : 0;
-                        k_idx[i] = x_padded_start;
-                    }
-
-                    connx_Iterator x_patch_iter;
-                    connx_Iterator_init(&x_patch_iter, feature_dim, x_patch_slices);
-
-                    // max pool
-                    int32_t x_patch_batch = connx_Iterator_get_batch_size(&x_patch_iter, feature_shape);
-                    connx_Iterator_rewind(&x_patch_iter, x_patch_batch);
-
-                    int32_t kernel_offset = -1;
-                    if (x_patch_batch == 1) {
-                        while (connx_Iterator_next(&x_patch_iter, x_patch_batch)) {
-                            int32_t x_patch_offset = connx_Iterator_offset(&x_patch_iter, feature_shape);
-                            // clang-format off
-                            {{TYPE}} tmp_y = X_flatten[x_patch_offset];
-                            // clang-format on
-                            if (kernel_offset < 0 || tmp_y > y) {
-                                y = tmp_y;
-                                kernel_offset = X_offset + x_patch_iter.idx;
-                            }
-                        }
-                    } else {
-                        while (connx_Iterator_next(&x_patch_iter, x_patch_batch)) {
-                            int32_t x_patch_offset = connx_Iterator_offset(&x_patch_iter, feature_shape);
-                            // clang-format off
-                            {{TYPE}} tmp_y = CONNX_{{DTYPE}}_MIN;
-                            // clang-format on
-                            for (int i = 0; i < x_patch_batch; i++) {
-                                if (X_flatten[x_patch_offset + i] > tmp_y) {
-                                    tmp_y = X_flatten[x_patch_offset + i];
-                                }
-                            }
-                            // clang-format off
-                            int32_t tmp_kernel_offset = connx_{{DTYPE | to_name }}_argmax(x_patch_batch, &tmp_y, X_flatten + x_patch_offset);
-                            // clang-format on
-
-                            if (kernel_offset < 0 || tmp_y > y) {
-                                y = tmp_y;
-                                kernel_offset = X_offset + x_patch_iter.idx + tmp_kernel_offset;
-                            }
-                        }
-                    }
-
-                    // Convert offset to index
-                    for (int32_t i = 1; i < feature_dim; i++) {
-                        int32_t unit = connx_Int32_product(feature_dim - i, x_patch_iter.subshape + i);
-                        k_idx[i - 1] += kernel_offset / unit;
-                        kernel_offset = kernel_offset % unit;
-                    }
-                    k_idx[feature_dim - 1] += kernel_offset;
-
-                    int32_t d_idx[feature_dim];
-                    int32_t d_offset = 0;
-
-                    // Compute offset of max argmax in X[batch, channel]
-                    for (int32_t i = 0; i < feature_dim; i++) {
-                        d_idx[i] = x_idxs[i] + k_idx[i];
-                        d_offset += d_idx[i] * units[2 + i];
-                    }
-
-                    argmax_idx = d_offset;
-                    Y_flatten[y_idx] = y;
-
-                    if (output_count > 1) {
-                        if (storage_order == 1) {
-                            int32_t remainder = y_idx % storage_unit;
-                            int32_t share = y_idx - remainder;
-                            int32_t a = remainder * output_shape[feature_dim - 1];
-                            Indices_array[share + a % storage_unit + (int32_t)(a / storage_unit)] = argmax_idx;
-                        } else {
-                            Indices_array[y_idx] = argmax_idx;
-                        }
-                    }
-                    y_idx++;
-                }
-
-                X_offset += X_unit;
-                X_flatten += X_unit;
             }
         }
         break;
@@ -419,6 +273,7 @@ int MaxPool_{{op_version}}(connx_Graph* graph, uint32_t output_count, uint32_t* 
         return CONNX_NOT_SUPPORTED_DATATYPE;
     }
 
+    // Transpose Indices if storage_order == 1
     if (storage_order == 1 && feature_dim >= 2) {
         int32_t height = output_shape[feature_dim - 2];
         int32_t width = output_shape[feature_dim - 1];
