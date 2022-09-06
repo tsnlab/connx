d44e1615b6ffa37fbb96467b2140db81a049fa34
Myoungwoo Jang <myoungwoo@tsnlab.com>
2022-09-06 17:13:10 +0900

    fix: Correct unidirectional broadcast for GEMM-13  - Check if bias can be uni-broadcastable  - Add missing case for bias uni-broadcasting
    style: Remove debug message

--- src/opset/Gemm_13.c
+++ src/opset/Gemm_13.c
@@ -23,23 +23,6 @@
 #include <connx/accel.h>
 #include <connx/connx.h>
 
-#define GEMM_DEBUG 0
-
-__attribute__((unused)) static void show_elements_tensor(char* name, int32_t rows, int32_t cols, float32_t* d,
-                                                         bool transposed) {
-    fprintf(stderr, "%s %s Transposed ( %d, %d)\n", name, transposed ? "" : "Not", rows, cols);
-    int32_t num = transposed ? rows : cols;
-    float32_t(*m)[num] = (float32_t(*)[num])d;
-    for (int32_t r = 0; r < rows; r++) {
-        for (int32_t c = 0; c < cols; c++) {
-            float32_t val = transposed ? m[c][r] : m[r][c];
-            fprintf(stderr, "%f ", val);
-        }
-        fprintf(stderr, "\n");
-    }
-    fprintf(stderr, "\n");
-}
-
 // clang-format off
 int Gemm_{{op_version}}(connx_Graph* graph, __attribute__((unused)) uint32_t output_count, uint32_t* outputs,
                          // clang-format on
@@ -58,69 +41,60 @@ int Gemm_{{op_version}}(connx_Graph* graph, __attribute__((unused)) uint32_t out
     float32_t(*a)[A->shape[1]] = A->buffer;
     float32_t(*b)[B->shape[1]] = B->buffer;
 
-    connx_Tensor* C = NULL;
-    bool is_biased = false;
-
-    if (input_count == 3) {
-        C = connx_Graph_get(graph, inputs[2]);
-        assert(C != NULL);
-        is_biased = true;
-    }
-
     // alias for A, B rows/cols whether transposed flags enabled
     int32_t A_rows = A->shape[0];
     int32_t A_cols = A->shape[1];
     int32_t B_rows = B->shape[0];
     int32_t B_cols = B->shape[1];
-    if (transA && transB) {
+
+    if (transA) {
         A_rows = A->shape[1];
         A_cols = A->shape[0];
-        B_rows = B->shape[1];
-        B_cols = B->shape[0];
-    } else if (transA && !transB) {
-        A_rows = A->shape[1];
-        A_cols = A->shape[0];
-    } else if (!transA && transB) {
+    }
+
+    if (transB) {
         B_rows = B->shape[1];
         B_cols = B->shape[0];
     }
 
     // A's # of cols and B's # of row should be same
     if (A_cols != B_rows) {
-        connx_error("Gemm: A->shape[1] != B->shape[0]");
+        connx_error("Gemm: A_cols != B_rows");
         return CONNX_TENSOR_SHAPE_NOT_MATCHING;
     }
 
+    connx_Tensor* C = NULL;
+    bool is_biased = false;
+    if (input_count == 3) {
+        C = connx_Graph_get(graph, inputs[2]);
+        assert(C != NULL);
+        // Check if Unidirectional Broadcasting is possible,
+        // result shape should be (A_rows, B_cols)
+        if (A_rows == C->shape[0] && B_cols == C->shape[1]) { // same shape
+            is_biased = true;
+        } else if (C->ndim == 0) { // scalar type
+            is_biased = true;
+        } else if (C->ndim == 1 || C->shape[1] == B_cols) {
+            is_biased = true;
+        } else if (C->ndim == 2) {
+            if ((C->shape[0] == 1 && C->shape[1] == B_cols) || (C->shape[1] == 1 && C->shape[0] == A_rows)) {
+                is_biased = true;
+            }
+        }
+
+        if (!is_biased) {
+            connx_error("Gemm: Unidirectional Broadcasting is not possible");
+            return CONNX_TENSOR_SHAPE_NOT_MATCHING;
+        }
+    }
+
     // initialize output tensor
-    int ndim = A->ndim; // Always 2D
+    int ndim = 2; // Always 2D
     int32_t shape[] = {A_rows, B_cols};
     connx_Tensor* Y = connx_Tensor_alloc(A->dtype, ndim, shape);
     if (Y == NULL) {
         return CONNX_NOT_ENOUGH_MEMORY;
     }
-
-#if GEMM_DEBUG // TODO: erase this code
-    fprintf(stderr, "\n");
-    connx_debug("Gemm: Y <- 𝛼AB + βC\n");
-    connx_debug("Gemm input_count: %d, attribute_count %d\n", input_count, attribute_count);
-    connx_debug("Gemm: alpha %f, beta %f, transA %d, transB %d\n\n", alpha, beta, transA, transB);
-    connx_debug("A ndim %d ", A->ndim);
-    connx_Tensor_dump(A);
-    connx_debug("B ndim %d ", B->ndim);
-    connx_Tensor_dump(B);
-    if (C != NULL) {
-        connx_debug("C ndim %d ", C->ndim);
-        connx_Tensor_dump(C);
-    }
-    connx_debug("Gemm: A_rows %d, A_cols %d, B_rows %d, B_cols %d\n", A_rows, A_cols, B_rows, B_cols);
-
-    if (transA) {
-        show_elements_tensor("Tensor A", A_rows, A_cols, A->buffer, transA);
-    }
-    if (transB) {
-        show_elements_tensor("Tensor B", B_rows, B_cols, B->buffer, transB);
-    }
-#endif
 
     switch (A->dtype) {
         /*{% set type_constraints = [
@@ -151,15 +125,19 @@ int Gemm_{{op_version}}(connx_Graph* graph, __attribute__((unused)) uint32_t out
                     {{TYPE}} b_val = transB ? b[col][k] : b[k][col];
                     // clang-format on
                     sum += alpha * a_val * b_val;
-                    // fprintf(stderr, "a[%d][%d] b[%d][%d]\n", r, k, k, c);
                 }
-                // fprintf(stderr, "%f\n", sum);
+
                 if (is_biased) {
                     {{TYPE}}(*c)[C->shape[1]] = C->buffer;
-                    if (C->ndim == 0 || (C->ndim == 1 && C->shape[0] == 1)) {
-                        // already assigned once above, so do nothing
+                    if (C->ndim == 0 || (C->ndim == 1 && C->shape[0] == 1) ||
+                        (C->ndim == 2 && C->shape[0] == 1 && C->shape[1] == 1)) {
+                        // already assigned for single value once above, so do nothing
+                    } else if (C->ndim == 1 && C->shape[0] > 1) {
+                        bias = (({{TYPE}}*)C->buffer)[col];
                     } else if (C->ndim == 2 && C->shape[0] == 1) {
                         bias = (({{TYPE}}*)C->buffer)[col];
+                    } else if (C->ndim == 2 && C->shape[1] == 1) {
+                        bias = (({{TYPE}}*)C->buffer)[row];
                     } else {
                         bias = c[row][col];
                     }
@@ -177,10 +155,6 @@ int Gemm_{{op_version}}(connx_Graph* graph, __attribute__((unused)) uint32_t out
         connx_error("Gemm: Datatype %d is not supported yet.\n", A->dtype);
         return CONNX_NOT_SUPPORTED_DATATYPE;
     }
-#if GEMM_DEBUG
-    connx_debug("Y ndim %d ", Y->ndim);
-    connx_Tensor_dump(Y);
-#endif
 
     connx_Graph_set(graph, outputs[0], Y);
     return CONNX_OK;
