e0f8759eccdd1b9e337ceb51923d9129976f0916
Jeong Arm <kjwonmail@gmail.com>
2022-11-29 13:37:08 +0900

    Fix clang-format-11 lint

--- src/opset/Floor_13.c
+++ src/opset/Floor_13.c
@@ -22,9 +22,9 @@
 
 // clang-format off
 int Floor_{{op_version}}(connx_Graph* graph, __attribute__((unused)) uint32_t output_count, uint32_t* outputs,
-                         // clang-format on
-                         __attribute__((unused)) uint32_t input_count, uint32_t* inputs,
-                         __attribute__((unused)) uint32_t attribute_count, __attribute__((unused)) void** attributes) {
+                          // clang-format on
+                          __attribute__((unused)) uint32_t input_count, uint32_t* inputs,
+                          __attribute__((unused)) uint32_t attribute_count, __attribute__((unused)) void** attributes) {
     connx_Tensor* X = connx_Graph_get(graph, inputs[0]);
     connx_Tensor* Y = connx_Tensor_alloc_like(X);
     if (Y == NULL) {
